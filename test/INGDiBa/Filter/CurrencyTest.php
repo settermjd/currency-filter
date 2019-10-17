ddddd6bcebe80c0f26fd7782f4a684c1c9a61857
Matthew Setter <matthew.setter@gmail.com>
2019-10-17 09:34:09 +0200

    Create a filter for filtering ING DiBa currency values
    This is a minimalist filter class that can integrate with
    zendframework/zend-filter which can return a filtered ING DiBa currency
    value, one written in their CSV export files.

---
+++ test/INGDiBa/Filter/CurrencyTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace INGDiBaTest\Filter;
+
+use INGDiBa\Filter\Currency;
+use PHPUnit\Framework\TestCase;
+use Zend\Filter\Exception\InvalidArgumentException;
+
+/**
+ * Class CurrencyTest
+ * @package INGDiBaTest\Filter
+ * @coversDefaultClass \INGDiBa\Filter\Currency
+ */
+class CurrencyTest extends TestCase
+{
+    /**
+     * @param string $testData
+     * @param string $validData
+     * @dataProvider dataProvider
+     */
+    public function testCanCorrectlyFilterInputData($testData, $validData)
+    {
+        $filter = new Currency();
+        $this->assertEquals($validData, $filter->filter($testData));
+    }
+
+    public function dataProvider()
+    {
+       return [
+           ['1000,00', 100000],
+           ['1234,4', 123440],
+           ['1234,', 123400],
+           ['887761234,', 88776123400],
+       ];
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testThrowsExceptionIfStringDoesNotContainASeparator()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Supplied currency value does not match the required format.');
+        $filter = new Currency();
+        $filter->filter(1234);
+    }
+
+    public function invalidDataProvider()
+    {
+        return [
+            [1234],
+            ['1234'],
+            [1234.43],
+            ['1234.43'],
+            ['1234,43987'],
+            [-1234],
+            [-1234.54],
+            [-1234.2],
+        ];
+    }
+}
