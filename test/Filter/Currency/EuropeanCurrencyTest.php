030b761ba2d11350e4a851370727ce37dc6aa49a
Matthew Setter <matthew@matthewsetter.com>
2019-10-19 22:55:39 +0200

    Rename Currency class to EuropeanCurrency
    After a bit of thought, it seemed clear that I was being ultra-specific
    with the original class, and package, name, so much so and didn't need
    to be. The grounds for that is that what I was ultimately checking for
    was a European currency, not something specific to ING DiBa. As such,
    the classes and package have been renamed appropriately.

---
+++ test/Filter/Currency/EuropeanCurrencyTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace settermjd\Filter\Currency\Test\Currency;
+
+use settermjd\Filter\Currency\EuropeanCurrency;
+use PHPUnit\Framework\TestCase;
+use Zend\Filter\Exception\InvalidArgumentException;
+
+/**
+ * Class EuropeanCurrencyTest
+ * @package Filter\Currency\Test
+ * @coversDefaultClass \settermjd\Filter\Currency\EuropeanCurrency
+ */
+class EuropeanCurrencyTest extends TestCase
+{
+    /**
+     * @param string $testData
+     * @param string $validData
+     * @dataProvider dataProvider
+     */
+    public function testCanCorrectlyFilterInputData($testData, $validData)
+    {
+        $filter = new EuropeanCurrency();
+        $this->assertEquals($validData, $filter->filter($testData));
+    }
+
+    public function dataProvider()
+    {
+        return [
+            ['1,01', 101],
+            ['1,2', 120],
+            ['1,', 100],
+            ['20,21', 2021],
+            ['21,02', 2102],
+            ['21,2', 2120],
+            ['1000,00', 100000],
+            ['1.00,00', 10000],
+            ['-3500,00', -350000],
+            ['-3.500,01', -350001],
+            ['-4.501,2', -450120],
+            ['1.887.761.234', 188776123400],
+            ['1.887.761.234,01', 188776123401],
+            ['1.887.761.234,2', 188776123420],
+            ['1887761234,2', 188776123420],
+            ['1887761234,21', 188776123421],
+            ['1887761234,2', 188776123420],
+            ['1887761234,', 188776123400],
+            ['1234.24', 12342400],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testThrowsExceptionIfStringDoesNotMatchTheRequiredPattern($data)
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Supplied currency value does not match the required format.');
+        $filter = new EuropeanCurrency();
+        $filter->filter($data);
+    }
+
+    public function invalidDataProvider()
+    {
+        return [
+            ['1234,43987'],
+            ['Lorem ipsum dolor sit amet, consectetur adipiscing elit.'],
+            [null],
+            [''],
+            [false],
+        ];
+    }
+}
